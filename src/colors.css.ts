e7435165124d8770ab53293931f2c7bcc8bfd5bb
Saad Wambangulu <saad.wambangulu@grumetimedia.co.tz>
2022-11-26 13:26:55 +0300

    First full website commit

---
+++ src/colors.css.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  background: "#1c2939", /* #ffd500*/ 
+  text: "#ffffff", /* #005bbb*/
+  primary: "#ffffff", /* #005bbb*/
+  muted: "#454f5b", /* #f5cc00*/
+  active: "#004287",
+  black: "#000",
+}
